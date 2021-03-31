dcc3bcf64f55c948235eb4ec7e7226c75ccd3285
priyanka <pdethe67@example.com>
2021-03-31 14:46:45 +0000

    correction done

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
 	System.out.println("wrong command");
+	//hello
     }
 //this is just a comment
 }
